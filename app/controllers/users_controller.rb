7f4352b1b83c5b729533bb0cae97a3b110d6dae1
yuya-ponkotu <yuya@yuya.com>
2022-02-27 17:26:58 +0900

    create user action update delete create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+      redirect_to root_path
+    else
+      flash[:danger] = "名前が入力されてません"
+      redirect_to new_user_path
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to root_path
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Test 1",
+             entry_time: Time.zone.now,
+             leave_time: Time.zone.now)
+
+User.create!(name:  "Test 2",
+             entry_time: Time.zone.now,
+             leave_time: Time.zone.now)
